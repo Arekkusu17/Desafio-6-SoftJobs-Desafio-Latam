a2633d65b855a632d62fd579dcdcfbeca17b6f31
Arekkusu17 <fernn.alex@gmail.com>
2023-06-15 00:23:25 -0400

    create and use middlewares

--- middlewares/reportRequest.js
+++ middlewares/reportRequest.js
@@ -4,6 +4,6 @@ export const reportQuery = async (req, res, next) => {
   const url = req.url
   console.log(`
   Hoy ${new Date()}
-  Se ha recibido una consulta en la ruta ${url}`)
+  Se ha recibido una petición en la ruta ${url}`)
   next() // informa el codigo y continua al siguiente bloque
 }
